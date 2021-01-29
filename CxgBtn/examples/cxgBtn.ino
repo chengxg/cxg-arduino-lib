e157589c2d6b830ce546ca2dabd1f8022980f891
chengxg <1610715813@qq.com>
2021-01-29 20:52:18 +0800

    提交CxgBtndemo更改

--- CxgBtn/examples/cxgBtn.ino
+++ CxgBtn/examples/cxgBtn.ino
@@ -2,10 +2,13 @@
 #include "cxg_Btn.h"
 
 static int btn1_pin = 39;
-static CxgBtn btn1(btn1_pin, false);
+static CxgBtn btn1;
 
 void setup() {
   Serial.begin(115200);
+
+  //由构造函数初始化变为, 调用attch方法初始化
+  btn1.attach(btn1_pin, false);
 
   //按钮按下回调
   btn1.setBtnKeydown([]() {
